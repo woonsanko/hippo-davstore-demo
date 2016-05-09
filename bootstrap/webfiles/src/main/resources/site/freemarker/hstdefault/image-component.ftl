e7a6aaeb1b82d71d63a3fb26a82e77f8529a8a82
Woonsan Ko <w.ko@onehippo.com>
2016-05-09 08:03:57 -0700

    initial setup of hippo cms project

---
+++ bootstrap/webfiles/src/main/resources/site/freemarker/hstdefault/image-component.ftl
@@ -0,0 +1,11 @@
+<#include "../include/imports.ftl">
+
+<#-- @ftlvariable name="document" type="org.hippoecm.hst.content.beans.standard.HippoGalleryImageSetBean" -->
+<#if document??>
+<@hst.link var="img" hippobean=document.original/>
+    <img src="${img}" title="${document.fileName?html}" alt="${document.fileName?html}"/>
+<#-- @ftlvariable name="editMode" type="java.lang.Boolean"-->
+<#elseif editMode>
+    <img src="<@hst.link path="/images/essentials/catalog-component-icons/image.png" />"> Click to edit Image
+</#if>
+
---
+++ site/src/main/java/com/github/woonsanko/davstore/demo/beans/Banner.java
@@ -0,0 +1,46 @@
+package com.github.woonsanko.davstore.demo.beans;
+/*
+ * Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+
+@HippoEssentialsGenerated(internalName = "hippodavstoredemo:bannerdocument")
+@Node(jcrType = "hippodavstoredemo:bannerdocument")
+public class Banner extends BaseDocument {
+	@HippoEssentialsGenerated(internalName = "hippodavstoredemo:title")
+	public String getTitle() {
+		return getProperty("hippodavstoredemo:title");
+	}
+
+	@HippoEssentialsGenerated(internalName = "hippodavstoredemo:content")
+	public HippoHtml getContent() {
+		return getHippoHtml("hippodavstoredemo:content");
+	}
+
+	@HippoEssentialsGenerated(internalName = "hippodavstoredemo:image")
+	public HippoGalleryImageSet getImage() {
+		return getLinkedBean("hippodavstoredemo:image", HippoGalleryImageSet.class);
+	}
+
+	@HippoEssentialsGenerated(internalName = "hippodavstoredemo:link")
+	public HippoBean getLink() {
+		return getLinkedBean("hippodavstoredemo:link", HippoBean.class);
+	}
+}
