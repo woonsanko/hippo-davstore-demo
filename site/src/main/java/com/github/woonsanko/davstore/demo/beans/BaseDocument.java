9ee8cd21ebff462976708642271c1628f5bae956
Woonsan Ko <woonsan.ko@bloomreach.com>
2018-08-07 10:32:48 -0400

    removing old hippo files to use the latest

--- site/src/main/java/com/github/woonsanko/davstore/demo/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package com.github.woonsanko.davstore.demo.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="hippodavstoredemo:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
