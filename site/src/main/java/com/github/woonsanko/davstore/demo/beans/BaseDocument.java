560180fc2ae3e10ed1fbdad6f6599812817326e5
Woonsan Ko <woonsan.ko@bloomreach.com>
2018-08-07 12:02:03 -0400

    Recreate hippo demo based on 12.4

---
+++ site/src/main/java/com/github/woonsanko/davstore/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.github.woonsanko.davstore.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="hippodavstoredemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
